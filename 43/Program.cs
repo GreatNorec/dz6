d7cee9f671095de8c3b7ca60b6147373ea4cfacb
GN <andreifootball@yandex.ru>
2023-03-23 17:21:47 +0500

    43 done

---
+++ 43/Program.cs
@@ -0,0 +1,18 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 => 2
+//1, -7, 567, 89, 223 => 4
+
+Console.Write("Введите какое-то кол-во чисел через запятую: ");
+string array = Console.ReadLine().Replace(",", "");
+
+int[] arr = Array.ConvertAll( array.Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Кол-во элементов больше 0: {count}");
